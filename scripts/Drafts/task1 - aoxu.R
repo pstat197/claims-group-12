fb5db4d0911dcca87fcaa5b3ad00aec859661574
bobaoxu2001 <ao_xu@umail.ucsb.edu>
2022-11-10 14:09:55 -0800

    Updated
    Updated

--- scripts/Drafts/task1 - aoxu.R
+++ scripts/Drafts/task1 - aoxu.R
@@ -1,10 +1,3 @@
-# setup
-library(tidyverse)
-library(tidytext)
 library(tokenizers)
-library(textstem)
-library(stopwords)
 
-# read data
-load('/Users/xuao/Documents/2022Fall/Pstat197/claims-group-12/data/claims-raw.RData')
-claims-row
+invitation_text %>% tokenize_words()
