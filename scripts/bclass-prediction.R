1703d245e73d994972a4b77997345aba3bef79c5
acmacomber <76853073+acmacomber@users.noreply.github.com>
2022-11-14 12:32:40 -0800

    Changes to preprocessing for bclass prediction

---
+++ scripts/bclass-prediction.R
@@ -0,0 +1,87 @@
+# Primary task a) binary class prediction
+
+## PREPROCESSING
+#################
+source('scripts/preprocessing copy.R')
+
+# load raw data
+load('data/claims-raw.RData')
+
+# preprocess (will take a minute or two)
+claims_clean <- claims_raw %>%
+  parse_data()
+
+# export
+save(claims_clean, file = 'data/claims-clean-example.RData')
+
+## MODEL TRAINING (NN)
+######################
+library(tidyverse)
+library(tidymodels)
+library(keras)
+library(tensorflow)
+
+# load cleaned data
+load('data/claims-clean-example.RData')
+
+# partition
+set.seed(111422)
+partitions <- claims_clean %>%
+  initial_split(prop = 0.7)
+
+train_text <- training(partitions) %>%
+  pull(text_clean)
+train_labels <- training(partitions) %>%
+  pull(bclass) %>%
+  as.numeric() - 1
+
+test_text <- testing(partitions) %>%
+  pull(text_clean)
+test_labels <- testing(partitions) %>%
+  pull(bclass) %>%
+  as.numeric() - 1
+
+# create a preprocessing layer
+preprocess_layer <- layer_text_vectorization(
+  standardize = NULL,
+  split = 'whitespace',
+  ngrams = NULL,
+  max_tokens = NULL,
+  output_mode = 'tf_idf'
+)
+
+preprocess_layer %>% adapt(train_text)
+
+
+# changed layer_dropout from 0.25 to 0.75
+# define NN architecture
+model <- keras_model_sequential() %>%
+  preprocess_layer() %>%
+  layer_dropout(0.75) %>%
+  layer_dense(units = 50) %>%
+  layer_dropout(0.25) %>%
+  layer_dense(1) %>%
+  layer_activation(activation = 'sigmoid')
+
+summary(model)
+
+# configure for training
+model %>% compile(
+  loss = 'binary_crossentropy',
+  optimizer = 'adam',
+  metrics = 'binary_accuracy'
+)
+
+# train
+history <- model %>%
+  fit(train_text, 
+      train_labels,
+      validation_split = 0.3,
+      epochs = 5)
+
+## CHECK TEST SET ACCURACY HERE
+model$weights
+evaluate(model, test_text, test_labels)
+
+# model has accuracy of 0.81 which is higher than 
+# the accuracy of 0.77 we got in task 1
