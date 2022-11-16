08e226101a9c005b636ed6ec69cb4daabebe6810
bobaoxu2001 <ao_xu@umail.ucsb.edu>
2022-11-15 18:25:02 -0800

    updated

--- scripts/Drafts/multi-class testing - aoxu.R
+++ scripts/Drafts/multi-class testing - aoxu.R
@@ -1,3 +1,79 @@
+source('https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/scripts/package-installs.R')
+
+# packages
+library(tidyverse)
+library(tidymodels)
+library(modelr)
+library(Matrix)
+library(sparsesvd)
+library(glmnet)
+
+# path to activity files on repo
+url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/activities/data/'
+
+# load a few functions for the activity
+source(paste(url, 'projection-functions.R', sep = ''))
+
+# read in data
+claims <- paste(url, 'claims-multi-tfidf.csv', sep = '') %>%
+  read_csv()
+
+# preview
+claims
+
+# 1
+# partition data
+set.seed(102722)
+partitions <- claims %>% initial_split(prop = 0.8)
+
+# separate DTM from labels
+test_dtm <- testing(partitions) %>%
+  select(-.id, -bclass, -mclass)
+test_labels <- testing(partitions) %>%
+  select(.id, bclass, mclass)
+
+# same, training set
+train_dtm <- training(partitions) %>%
+  select(-.id, -bclass, -mclass)
+train_labels <- training(partitions) %>%
+  select(.id, bclass, mclass)
+
+# find projections based on training data
+proj_out <- projection_fn(.dtm = train_dtm, .prop = 0.7)
+train_dtm_projected <- proj_out$data
+
+# how many components were used?
+proj_out$n_pc
+
+train <- train_labels %>%
+  transmute(bclass = factor(bclass)) %>%
+  bind_cols(train_dtm_projected)
+
+fit <- glm(..., data = train, ...)
+
+# store predictors and response as matrix and vector
+x_train <- train %>% select(-bclass) %>% as.matrix()
+y_train <- train_labels %>% pull(bclass)
+
+# fit enet model
+alpha_enet <- 0.3
+fit_reg <- glmnet(x = x_train, 
+                  y = y_train, 
+                  family = 'binomial',
+                  alpha = alpha_enet)
+
+# choose a strength by cross-validation
+set.seed(102722)
+cvout <- cv.glmnet(x = x_train, 
+                   y = y_train, 
+                   family = 'binomial',
+                   alpha = alpha_enet)
+
+# store optimal strength
+lambda_opt <- cvout$lambda.min
+
+# view results
+cvout
 ## Step 1: multinomial regression
 #### get multiclass labels
 y_train_multi <- train_labels %>% pull(mclass)
