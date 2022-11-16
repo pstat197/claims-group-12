1327c815ab4a7f56e7bb8027214f39c2bab92055
lyndseyumsted <lyndseyumsted@ucsb.edu>
2022-11-15 19:37:18 -0800

    can someone try running this

--- scripts/Drafts/task1-lyndseyumsted.R
+++ scripts/Drafts/task1-lyndseyumsted.R
@@ -20,6 +20,41 @@ url <- 'https://raw.githubusercontent.com/pstat197/pstat197a/main/materials/acti
 # load a few functions for the activity
 source(paste(url, 'projection-functions.R', sep = ''))
 
+### compressing #####
+stpwrd <- stop_words %>%
+  pull(word) %>%
+  str_remove_all('[[:punct:]]')
+
+
+clean_head <- claims_clean %>% 
+  unnest_tokens(output = token, # specifies new column name
+                input = text, # specifies column containing text
+                token = 'words', # how to tokenize
+                stopwords = stpwrd) %>% # optional stopword removal
+  mutate(token = lemmatize_words(token)) 
+
+
+nlp_fn <- function(parse_data.out){
+  out <- parse_data.out %>%
+    unnest_tokens(output = token,
+                  input = text_clean,
+                  token = 'words', ## need to switch to n grams
+                  stopwords = str_remove_all(stop_words$word,
+                                             '[[:punct:]]')) %>%
+    mutate(token.lem = lemmatize_words(token)) %>%
+    filter(str_length(token.lem) > 2) %>%
+    count(.id, bclass, token.lem, name = 'n') %>%
+    bind_tf_idf(term = token.lem,
+                document = .id,
+                n = n) %>%
+    pivot_wider(id_cols = c('.id', 'bclass'),
+                names_from = 'token.lem',
+                values_from = 'tf_idf',
+                values_fill = 0)
+  return(out)
+}
+
+
 # function to parse html and clean text
 parse_fn <- function(.html){
   read_html(.html) %>%
@@ -50,56 +85,32 @@ parse_data <- function(.df){
   return(out)
 }
 
-nlp_fn <- function(parse_data.out){
-  out <- parse_data.out %>% 
-    unnest_tokens(output = token, 
-                  input = text_clean, 
-                  token = 'words',
-                  stopwords = str_remove_all(stop_words$word, 
-                                             '[[:punct:]]')) %>%
-    mutate(token.lem = lemmatize_words(token)) %>%
-    filter(str_length(token.lem) > 2) %>%
-    count(.id, bclass, token.lem, name = 'n') %>%
-    bind_tf_idf(term = token.lem, 
-                document = .id,
-                n = n) %>%
-    pivot_wider(id_cols = c('.id', 'bclass'),
-                names_from = 'token.lem',
-                values_from = 'tf_idf',
-                values_fill = 0)
-  return(out)
-}
 
 # load raw data
 load('data/claims-raw.RData')
 # preprocess (will take a minute or two)
-claims_clean <- claims_raw %>%
-  parse_data()
-
-# export
-# save(claims_clean, file = 'data/claims-clean-example.RData')
+claims_clean <- parse_data(claims_raw) 
+text_data <- nlp_fn(claims_clean)
 
 
-
-# load cleaned data
-load('data/claims-clean-example.RData')
+#######
 
 # partition
 set.seed(110122)
-partitions <- claims_clean %>%
+partitions <- text_data %>%
   initial_split(prop = 0.8)
 
 # separate DTM from labels
 test_dtm <- testing(partitions) %>%
-  select(-.id, -bclass, -mclass)
+  select(-.id, -bclass)
 test_labels <- testing(partitions) %>%
-  select(.id, bclass, mclass)
+  select(.id, bclass)
 
 # same, training set
 train_dtm <- training(partitions) %>%
-  select(-.id, -bclass, -mclass)
+  select(-.id, -bclass)
 train_labels <- training(partitions) %>%
-  select(.id, bclass, mclass)
+  select(.id, bclass)
 
 
 # find projections based on training data
@@ -114,8 +125,58 @@ train <- train_labels %>%
   transmute(bclass = factor(bclass)) %>%
   bind_cols(train_dtm_projected)
 
-fit <- glm(bclass ~ ., data = train, family = binomial)
+# store predictors and response as matrix and vector
+x_train <- train %>% select(-bclass) %>% as.matrix()
+y_train <- train_labels %>% pull(bclass)
 
+# fit enet model
+alpha_enet <- 0.3
+fit_reg <- glmnet(x = x_train, 
+                  y = y_train, 
+                  family = 'binomial',
+                  alpha = alpha_enet)
+
+# choose a strength by cross-validation
+set.seed(102722)
+cvout <- cv.glmnet(x = x_train, 
+                   y = y_train, 
+                   family = 'binomial',
+                   alpha = alpha_enet)
+
+# store optimal strength
+lambda_opt <- cvout$lambda.min
+
+# project test data onto PCs
+test_dtm_projected <- reproject_fn(.dtm = test_dtm, proj_out)
+
+# coerce to matrix
+x_test <- as.matrix(test_dtm_projected)
+
+# compute predicted probabilities
+preds <- predict(fit_reg, 
+                 s = lambda_opt, 
+                 newx = x_test,
+                 type = 'response')
+
+# store predictions in a data frame with true labels
+pred_df <- test_labels %>%
+  transmute(bclass = factor(bclass)) %>%
+  bind_cols(pred = as.numeric(preds)) %>%
+  mutate(bclass.pred = factor(pred > 0.5, 
+                              labels = levels(bclass)))
+
+# define classification metric panel 
+panel <- metric_set(sensitivity, 
+                    specificity, 
+                    accuracy, 
+                    roc_auc)
+
+# compute test set accuracy
+head <- pred_df %>% panel(truth = bclass, 
+                  estimate = bclass.pred, 
+                  pred, 
+                  event_level = 'second')
+head
 
 
 
